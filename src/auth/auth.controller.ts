41074ddc60253f454da4a807978f1a1435311d22
igdanit <vov4ikmolo4ik@gmail.com>
2022-11-23 00:31:02 +0600

    implemented PipeGuard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { newUser, UserDTO } from 'src/users/dto/user.dto';
 import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
@@ -11,11 +12,20 @@ export class AuthController {
         private readonly authService: AuthService,
         ){}
 
+
+    // Add new user
     @Post('sign-up')
-    async signUp(@Body() user: newUser) {
-        return await this.usersService.addUser(user)
+    async signUp(@Body() user: newUser, @Res() res: Response) {
+        await this.usersService.addUser(user)
+        
+        res.statusCode = HttpStatus.CREATED;
+        res.send({
+            statusCode: HttpStatus.CREATED,
+            message: "User created",
+        })
     }
 
+    // Issue JWT
     @Post('sign-in')
     async signIn(@Body() user: UserDTO) {
         return await this.authService.login(user)
