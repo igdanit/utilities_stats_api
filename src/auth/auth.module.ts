c76ff15e9289319e3be2abf01f4b009b3d5a9fe1
igdanit <vov4ikmolo4ik@gmail.com>
2022-12-06 23:14:56 +0600

    dotend implicitly added

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,15 @@ import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     UsersModule,
     JwtModule.register({
-      secret: process.env['ACCESS_TOKEN_SECRET'],
+      secret: process.env.ACCESS_TOKEN_SECRET,
       signOptions: {expiresIn: '15m'}, 
     })
   ],
