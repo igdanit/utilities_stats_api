662b132c3d5a1e20b9a48f9fb88e56b98b3cec46
igdanit <vov4ikmolo4ik@gmail.com>
2022-12-12 00:22:21 +0600

    initial

---
+++ microservices/indications/src/config.py
@@ -0,0 +1,16 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    
+    MONGO_INITDB_ROOT_USERNAME: str
+    MONGO_INITDB_ROOT_PASSWORD: str
+    MONGO_INITDB_DATABASE: str
+
+    DATABASE_URL: str
+
+    class Config:
+        env_file = '../.env'
+        env_file_encoding = 'utf-8'
+
+
+settings = Settings()
