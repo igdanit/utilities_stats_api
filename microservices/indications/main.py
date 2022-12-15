0f145d3fc45b6b535ae24b0ed5cf5885c59c1b23
igdanit <vov4ikmolo4ik@gmail.com>
2022-12-16 00:57:00 +0600

    start server

--- microservices/indications/main.py
+++ microservices/indications/main.py
@@ -1,5 +1,18 @@
-def main():
-    pass
+import asyncio
+import grpc
+import grpc.aio
+
+from grpcServer import indications_pb2_grpc
+from grpcServer.service import IndicationsService
+
+async def main():
+    grpc.aio.init_grpc_aio()
+    server = grpc.aio.server()
+    server.add_insecure_port("[::]:50051")
+    indications_pb2_grpc.add_IndicationsServicer_to_server(IndicationsService(), server)
+
+    await server.start()
+    await server.wait_for_termination()
 
 if (__name__ == '__main__'):
-    main()
+    asyncio.run(main())
