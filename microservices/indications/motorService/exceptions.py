dc17e5210e0752680125ac7df2efab82d86d1441
igdanit <vov4ikmolo4ik@gmail.com>
2023-01-02 21:24:51 +0600

    added msg property to exception

--- microservices/indications/motorService/exceptions.py
+++ microservices/indications/motorService/exceptions.py
@@ -1,2 +1,6 @@
 class IndexPairAlreadyExist(BaseException):
     """Raised when an insert or update fails due to a duplicate key error."""
+
+    def __init__(self, *args: tuple, msg: str, **kwargs: dict) -> None:
+        self.msg = msg
+        super().__init__(*args)
