2d15edc030806b5f6eca296b55cb51f08abf1506
igdanit <vov4ikmolo4ik@gmail.com>
2023-01-02 21:23:32 +0600

    -

--- microservices/indications/utilities.py
+++ microservices/indications/utilities.py
@@ -4,14 +4,18 @@ from pymongo.errors import DuplicateKeyError
 from motorService.exceptions import IndexPairAlreadyExist
 
 from abc import ABC
-from typing import Any
+from typing import Any, TypeVar
 from loguru import logger
 
+T = TypeVar("T")
 
-# Acts like ES6 Proxy object
+
+""" Acts like ES6 Proxy object.
+    Class successor should implement field that will intercepted.
+    All intercepted field can whether invoke field of original object through self.target object """
 class Proxy(ABC):
-    def __init__(self, target) -> None:
-        self.target = target
+    def __init__(self, target: T) -> None:
+        self.target: T = target
 
     def __getattribute__(self, __name: str) -> Any:
         # Check does object itself has that attrubute
@@ -23,7 +27,18 @@ class Proxy(ABC):
         """
         if result == None:
             # Check maybe do ancestors have that method/attribute
-            result = super().__getattribute__(__name)
+            try:
+                result = super().__getattribute__(__name)
+            except AttributeError:
+                pass
+
+        if result == None:
+            # Get proxified object's property
+            try:
+                result = getattr(self.target, __name)
+            except AttributeError:
+                pass
+
         if result == None:
             raise AttributeError()
         return result
@@ -36,8 +51,17 @@ class MotorProxy(Proxy):
     async def insert_one(self, *args, **kwargs):
         try:
             await self.target.insert_one(*args, **kwargs)
-        except DuplicateKeyError:
-            raise IndexPairAlreadyExist()
+        except DuplicateKeyError as e:
+            raise IndexPairAlreadyExist(msg=e.error)
         except Exception as e:
             logger.error(e)
             raise e
+
+
+def singleton(cls):
+    instances = {}
+    def wrapper(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+    return wrapper
